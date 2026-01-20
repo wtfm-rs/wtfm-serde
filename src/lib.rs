1cfc41e0319aabc99ae9ebdcbee62f472f5bcc08
Sam Liu <ontouchstart@gmail.com>
2026-01-20 14:49:06 -0500

    WTFM for serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,13 @@
 #![doc(html_playground_url = "https://play.rust-lang.org/")]
-
+//! # WTFM for [serde]
+//! 
+//! Although this is an empty crate without any code, `cargo doc`
+//! by default build documentation for dependencies
+//!
+//! ```sh
+//! du -hs docs/doc/serde*
+//! 5.0M	docs/doc/serde
+//! 4.4M	docs/doc/serde_core
+//!
+//! ```
+//! We can navigate to them via the sidebar menu.
